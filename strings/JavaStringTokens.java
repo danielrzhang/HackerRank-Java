7d5119c4735587130d2cca1dd434e237b43f0c01
Daniel Zhang <45671923+danielrzhang@users.noreply.github.com>
2021-04-06 12:15:31 -0400

    Add files via upload

--- strings/JavaStringTokens.java
+++ strings/JavaStringTokens.java
@@ -6,14 +6,22 @@ public class JavaStringTokens {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		String str = in.nextLine();
-		String[] array = str.split("[ !,.?_\'@]");
+		String str = in.nextLine().trim();
 		
-		System.out.println(array.length);
-		
-		for (int i = 0; i < array.length; i++) {
-			System.out.println(array[i]);
+		if (str.length() > 0) {
+			String[] array = str.split("[ !,.?_\'@]+");
+			
+			System.out.println(array.length);
+			
+			for (int i = 0; i < array.length; i++) {
+				System.out.println(array[i]);
+			}
+			
+		} else {
+			System.out.println(0);
+			System.out.println("");
 		}
+		in.close();
 	}
 
 }
