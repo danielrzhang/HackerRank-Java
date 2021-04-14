9d6f8f4b4d2fa837a902b01c1a5b5f181bb7893f
Daniel Zhang <45671923+danielrzhang@users.noreply.github.com>
2021-04-14 16:28:56 -0400

    Add files via upload

---
+++ algorithms/CompareTheTriplets.java
@@ -0,0 +1,57 @@
+package algorithms;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class CompareTheTriplets {
+
+    // Complete the compareTriplets function below.
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+    	int aCount = 0, bCount = 0;
+    	
+    	for (int i = 0; i < a.size(); i++) {
+    		if (a.get(i) > b.get(i)) {
+    			aCount++;
+    		} else if (a.get(i) < b.get(i)) {
+    			bCount++;
+    		}
+    	}
+    	return new ArrayList<Integer>(Arrays.asList(aCount, bCount));
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = compareTriplets(a, b);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
---
+++ algorithms/DiagonalDifference.java
@@ -0,0 +1,66 @@
+package algorithms;
+import static java.util.stream.Collectors.toList;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+class Result {
+
+	/*
+	 * Complete the 'diagonalDifference' function below.
+	 *
+	 * The function is expected to return an INTEGER.
+	 * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+	 */
+
+	public static int diagonalDifference(List<List<Integer>> arr) {
+		int counter1 = 0;
+		int counter2 = 0;
+		
+		for (int i = 0; i < arr.size(); i++) {
+			counter1 += arr.get(i).get(i);
+			counter2 += arr.get(i).get(arr.size() - i - 1);
+		}
+	
+		return Math.abs(counter1 - counter2);
+	}
+
+}
+
+public class DiagonalDifference {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+		int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+		List<List<Integer>> arr = new ArrayList<>();
+
+		IntStream.range(0, n).forEach(i -> {
+			try {
+				arr.add(
+						Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+						.map(Integer::parseInt)
+						.collect(toList())
+						);
+			} catch (IOException ex) {
+				throw new RuntimeException(ex);
+			}
+		});
+
+		int result = Result.diagonalDifference(arr);
+
+		bufferedWriter.write(String.valueOf(result));
+		bufferedWriter.newLine();
+
+		bufferedReader.close();
+		bufferedWriter.close();
+	}
+}
