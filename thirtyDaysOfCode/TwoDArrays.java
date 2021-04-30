ce9b407be0b8cb191f24a07fb9ca34fb2d950a9c
Daniel Zhang <45671923+danielrzhang@users.noreply.github.com>
2021-04-30 11:45:15 -0400

    Add files via upload

---
+++ thirtyDaysOfCode/TwoDArrays.java
@@ -0,0 +1,40 @@
+package thirtyDaysOfCode;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TwoDArrays {
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int[][] arr = new int[6][6];
+
+		for (int i = 0; i < 6; i++) {
+			String[] arrRowItems = scanner.nextLine().split(" ");
+			scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+			for (int j = 0; j < 6; j++) {
+				int arrItem = Integer.parseInt(arrRowItems[j]);
+				arr[i][j] = arrItem;
+			}
+		}
+
+		int counter;
+		int maxCounter = Integer.MIN_VALUE;
+
+		for (int i = 1; i < arr.length - 1; i++) {
+			for (int j = 1; j < arr[i].length - 1; j++) {
+				counter = arr[i - 1][j - 1] + arr[i - 1][j] + arr[i - 1][j + 1] + arr[i][j] + arr[i + 1][j - 1] + arr[i + 1][j] + arr[i + 1][j + 1];
+
+				if (counter > maxCounter) {
+					maxCounter = counter;
+				}
+			}
+		}
+
+		System.out.println(maxCounter);
+
+		scanner.close();
+	}
+}
